d3f8e338c7447bb66849da0a49476ef6510d2f6e
ychinique <ychinique@gmail.com>
2011-01-29 17:05:39 +0000

    Implemented resource caching for images and audio.

--- egads-austin-java/src/GameCore.java
+++ egads-austin-java/src/GameCore.java
@@ -9,6 +9,7 @@ import java.awt.image.BufferedImage;
 import java.io.IOException;
 import java.io.InputStream;
 import java.net.URL;
+import java.util.HashMap;
 import java.util.TimerTask;
 
 import javax.imageio.ImageIO;
@@ -18,8 +19,13 @@ import javax.swing.JApplet;
 public class GameCore implements MouseListener, MouseMotionListener, KeyListener {
 	private static final long serialVersionUID = 543954373910725885L;
 	
+	private HashMap<String, BufferedImage> imageCache;
+	private HashMap<String, AudioClip> audioCache;
+	
 	public GameCore() {
 		super();
+		imageCache = new HashMap<String, BufferedImage>();
+		audioCache = new HashMap<String, AudioClip>();
 	}
 	
 	public void onInit() {
@@ -39,21 +45,37 @@ public class GameCore implements MouseListener, MouseMotionListener, KeyListener
 		throw new RuntimeException("Method not implemented!");
 	}
 	public BufferedImage getImage(String path) {
+		if (imageCache.containsKey(path))
+			return imageCache.get(path);
+		
 		InputStream stream = this.getClass().getResourceAsStream(path);
 		if (stream == null)
 			return null;
+		
+		BufferedImage img;
 		try {
-			return ImageIO.read(stream);
+			img = ImageIO.read(stream);
+			imageCache.put(path, img);
+			return img;
 		} catch (IOException e) {
 			e.printStackTrace();
 			return null;
 		}
 	}
 	public AudioClip getAudio(String path) {
+		if (audioCache.containsKey(path))
+			return audioCache.get(path);
+		
 		URL url = this.getClass().getResource(path);
 		if (url == null)
 			return null;
-		return JApplet.newAudioClip(url);
+		
+		AudioClip clip = JApplet.newAudioClip(url);
+		if (clip == null)
+			return null;
+		
+		audioCache.put(path, clip);
+		return clip;
 	}
 	public void saveScore(String player, int score) {
 		throw new RuntimeException("Method not implemented!");
