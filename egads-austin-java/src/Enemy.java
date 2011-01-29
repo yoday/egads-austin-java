580cc4a6a4a7a66632fa51741d6b7362ca9e4b36
becrloon <becrloon@yahoo.com>
2011-01-29 17:29:44 +0000

    added some info to enemy (mobility, position)

--- egads-austin-java/src/Enemy.java
+++ egads-austin-java/src/Enemy.java
@@ -1,5 +1,16 @@
 public abstract class Enemy extends Entity{
+	//some constants that enumerate the state of the
+	public static final int TADPOLE = 1;
+	public static final int HINDLEGS = 2;
+	public static final int NEARFROG = 3;
+	public static final int FROG = 4;
 	protected int cx,cy,myR;
+	public int getX(){
+		return cx;
+	}
+	public int getY(){
+		return cy;
+	}
 	public boolean isColliding(int x,int y,int r){
 		int dx2 = (x-cx); dx2 *= dx2;
 		int dy2 = (y-cy); dy2 *= dy2;
@@ -9,4 +20,6 @@ public abstract class Enemy extends Entity{
 	}
 	public abstract boolean isEdible();
 	public abstract int getPointsValue();
+	public abstract boolean isMobile();
+	
 }
