ce1d00926634d1396e46d9fa78cea9b24714201a
becrloon <becrloon@yahoo.com>
2011-01-29 05:01:52 +0000

    added init, food

---
+++ egads-austin-java/src/Food.java
@@ -0,0 +1,18 @@
+import java.awt.*;
+import java.lang.ref.*;
+
+public class Food extends Entity{
+	
+	public void draw(Graphics2D g2){
+		
+	}
+	public void update(){
+		
+	}
+	public void kill(int condition){
+		
+	}
+	public void init(GameCore gc){
+		
+	}
+}
