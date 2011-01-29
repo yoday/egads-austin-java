c92039be2e63c908e359ead91f89ddfaae9459eb
becrloon@yahoo.com <becrloon@yahoo.com@738875f4-99b6-ddcf-0cc5-a8a68dc07871>
2011-01-29 22:46:59 +0000

    new art, working scoreboard

--- egads-austin-java/src/ScoreBoard.java
+++ egads-austin-java/src/ScoreBoard.java
@@ -3,20 +3,20 @@ import java.awt.image.*;
 
 public class ScoreBoard extends Entity{
 	//location information
-	private static final int BARLOCX = 0;
-	private static final int BARLOCY = 0;
+	private static final int BARLOCX = 20;
+	private static final int BARLOCY = 20;
 	private static final int NUMLOCX = 10;
-	private static final int NUMLOCY = 30;
-	private static final int BARWIDTH = 100;
+	private static final int NUMLOCY = 70;
+	private static final int BARWIDTH = 600;
 	private static final int BARHEIGHT = 20;
-	private static final Color BARCOL = Color.WHITE;
+	private static final Color BARCOL = new Color(0,0,0,.5f);
 	private int NUMWIDTH;
 	//image names
 	private static String[] numimgnames = new String[]{"art/numbers/0.png","art/numbers/1.png","art/numbers/2.png","art/numbers/3.png","art/numbers/4.png","art/numbers/5.png","art/numbers/6.png","art/numbers/7.png","art/numbers/8.png","art/numbers/9.png"};
 	private static String[] onstatenames = new String[]{"art/forevolutionbar/1egg-color.png","art/forevolutionbar/2hatched-color.png","art/forevolutionbar/3hindlegs-color.png","art/forevolutionbar/4almostfrog-color.png","art/forevolutionbar/5frog-color.png"};
 	private static String[] offstatenames = new String[]{"art/forevolutionbar/1egg-bw.png","art/forevolutionbar/2hatched-bw.png","art/forevolutionbar/3hindlegs-bw.png","art/forevolutionbar/4almostfrog-bw.png","art/forevolutionbar/5frog-bw.png"};
 	//how many points required to proceed to the next state
-	private static int[] toNextState = new int[]{100,100,100,100,100};
+	private static int[] toNextState = new int[]{10,100,100,100,100};
 	//the actual images
 	private BufferedImage[] numbs;
 	private BufferedImage[] onstates;
@@ -34,26 +34,27 @@ public class ScoreBoard extends Entity{
 	public void draw(Graphics2D g2){
 		int barw = (BARWIDTH*curSt)/4;
 		if(curSt<4){
-			barw+=(int)((BARWIDTH*((1-(1.0f*curPt)/toNextState[curSt])))/4);
+			barw = barw + (int)((BARWIDTH*(toNextState[curSt]-curPt)/4)/((1.0f) * toNextState[curSt]));
 		}
 		Color tmp = g2.getColor();
 		g2.setColor(BARCOL);
 		g2.fillRect(BARLOCX,BARLOCY,barw,BARHEIGHT);
 		g2.setColor(tmp);
+		
 		barw = BARWIDTH /4;
 		int cxl = BARLOCX;
 		for(int i = 0;i<=curSt;i++){
-			g2.drawImage(onstates[i],cxl,BARLOCY,null);
+			g2.drawImage(onstates[i],cxl,BARLOCY,onstates[i].getWidth()/4,onstates[i].getHeight()/4,null);
 			cxl += barw;
 		}
 		for(int i = curSt+1;i<offstates.length;i++){
-			g2.drawImage(offstates[i],cxl,BARLOCY,null);
+			g2.drawImage(offstates[i],cxl,BARLOCY,offstates[i].getWidth()/4,offstates[i].getHeight()/4,null);
 			cxl += barw;
 		}
 		int nxl = NUMLOCX;
 		for(int i = 0;i<digits.length;i++){
-			g2.drawImage(numbs[digits[i]],nxl,NUMLOCY,null);
-			nxl += NUMWIDTH;
+			g2.drawImage(numbs[digits[i]],nxl,NUMLOCY,numbs[digits[i]].getWidth()/4,numbs[digits[i]].getHeight()/4,null);
+			nxl += NUMWIDTH/4;
 		}
 	}
 	public void update(){
@@ -82,7 +83,7 @@ public class ScoreBoard extends Entity{
 		over += num;
 		curPt -= num;
 		if(curPt<=0){
-			//p.evolve();
+			p.evolve();
 			curSt += 1;
 			if(curSt>= toNextState.length){
 				curSt = 0;
