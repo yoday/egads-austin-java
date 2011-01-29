9c2727d6bd694a0ed24a7f9c95741df7d975a8d7
lss6 <lss6@aol.com>
2011-01-29 06:28:28 +0000

    Changes in Player.

--- egads-austin-java/src/Player.java
+++ egads-austin-java/src/Player.java
@@ -1,12 +1,17 @@
 import java.awt.Graphics2D;
 
 public class Player extends Entity{
+	int x = 0; int y = 0;
 	boolean up,left,down,right;
-	int v;
+	int speed = 0;
+	double theta = 0;
+	double deltatheta = 0;
+	int score = 0;
+	
 
 	public void draw(Graphics2D g2) {
-		// TODO Auto-generated method stub
-		
+		g2.rotate(deltatheta);
+		deltatheta = 0;
 	}
 	
 	public void kill(int condition) {
@@ -14,8 +19,28 @@ public class Player extends Entity{
 		
 	}
 
+	
+	//Key Cases that should do nothing: UP && DOWN || LEFT && RIGHT
 	public void update() {
-		// TODO Auto-generated method stub
+		if(up){ // We have up thrust
+			speed = 10;
+		}
+		if(down)
+		{
+			speed = -10;
+		}
+		if(left){
+			theta += Math.PI/16;
+			deltatheta = Math.PI/16;
+		}
+		if(right){
+			theta -= Math.PI/16;
+			deltatheta = - Math.PI/16;
+		}
+		//Now for the movement.
+		x += speed* Math.cos(theta);
+		y += speed* Math.sin(theta);
+		
 		
 	}
 
@@ -23,13 +48,22 @@ public class Player extends Entity{
 		// TODO Auto-generated method stub
 		
 	}
-	public void upthrust(boolean down){
-		this.up = down; // pressing the up key
+	public void upthrust(boolean isDown){
+		this.up = isDown; // pressing the up key
 	}
-	public void downthrust(){
-		
-		
+	public void downthrust(boolean isDown){
+		this.down = isDown;
 	}
+	public void leftturn(boolean isDown){
+		this.left = isDown;
+	}
+	public void rightturn(boolean isDown){
+		this.right = isDown;
+	}
+	public void ChangeState(){
+		//go to next state.
+	}
+	
 	//upthrust, downthrust, Nostate CCW, Cw
 	
 }
