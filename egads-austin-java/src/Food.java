f141c06a20cf6d912d6836408c702e83e5dfcdf0
becrloon@yahoo.com <becrloon@yahoo.com@738875f4-99b6-ddcf-0cc5-a8a68dc07871>
2011-01-29 17:29:44 +0000

    added some info to enemy (mobility, position)

--- egads-austin-java/src/Food.java
+++ egads-austin-java/src/Food.java
@@ -1,12 +1,9 @@
 import java.awt.*;
 import java.awt.image.*;
+import java.util.*;
 
 public class Food extends Enemy{
-	//some constants that state at what phase this food can be eaten
-	public static final int TADPOLE = 1;
-	public static final int HINDLEGS = 2;
-	public static final int NEARFROG = 3;
-	public static final int FROG = 4;
+	
 	private static String[] tadimageNames = new String[]{};
 	private static int[] tadimgval = new int[]{};
 	private static String[] hinimageNames = new String[]{};
@@ -15,17 +12,42 @@ public class Food extends Enemy{
 	private static int[] nerimgval = new int[]{};
 	private static String[] frgimageNames = new String[]{};
 	private static int[] frgimgval = new int[]{};
+	private Random rand = new Random();
 	private BufferedImage bi;
 	private int score;
+	int index;
+	String imgnm;
+	int edible;
 	public Food(int edibility){
-		
+		edible = edibility;
+		switch(edibility){
+		case TADPOLE:
+			index = rand.nextInt(tadimageNames.length);
+			score = tadimgval[index];
+			imgnm = tadimageNames[index];
+			break;
+		case HINDLEGS:
+			index = rand.nextInt(hinimageNames.length);
+			score = hinimgval[index];
+			imgnm = hinimageNames[index];
+			break;
+		case NEARFROG:
+			index = rand.nextInt(nerimageNames.length);
+			score = nerimgval[index];
+			imgnm = nerimageNames[index];
+			break;
+		case FROG:
+			index = rand.nextInt(frgimageNames.length);
+			score = frgimgval[index];
+			imgnm = frgimageNames[index];
+			break;
+		default: throw new IllegalArgumentException("Edibility must be from TADPOLE to FROG");
+		}
 	}
 	public void draw(Graphics2D g2){
 		
 	}
-	public void update(){
-		
-	}
+	public void update(){}
 	public void kill(int condition){
 		
 	}
@@ -36,6 +58,9 @@ public class Food extends Enemy{
 		return false;
 	}
 	public int getPointsValue(){
-		return 0;
+		return score;
+	}
+	public boolean isMobile(){
+		return false;
 	}
 }
