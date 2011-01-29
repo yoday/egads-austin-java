e0485c5b3137f00bfe798bb289c5d9a676108a20
lss6 <lss6@aol.com>
2011-01-29 05:04:14 +0000

    Still adding methods.

--- egads-austin-java/src/Player.java
+++ egads-austin-java/src/Player.java
@@ -1,3 +1,35 @@
-public abstract class Player extends Entity{
+import java.awt.Graphics2D;
+
+public class Player extends Entity{
+	boolean up,left,down,right;
+	int v;
+
+	public void draw(Graphics2D g2) {
+		// TODO Auto-generated method stub
+		
+	}
+	
+	public void kill(int condition) {
+		// TODO Auto-generated method stub
+		
+	}
+
+	public void update() {
+		// TODO Auto-generated method stub
+		
+	}
+
+	public void init(GameCore gc) {
+		// TODO Auto-generated method stub
+		
+	}
+	public void upthrust(boolean down){
+		this.up = down; // pressing the up key
+	}
+	public void downthrust(){
+		
+		
+	}
+	//upthrust, downthrust, Nostate CCW, Cw
 	
 }
