569273d4c0aa5a3a837ff25cf5d358856e6940b1
becrloon@yahoo.com <becrloon@yahoo.com@738875f4-99b6-ddcf-0cc5-a8a68dc07871>
2011-01-30 01:14:33 +0000

    food makes sound

--- egads-austin-java/src/Food.java
+++ egads-austin-java/src/Food.java
@@ -7,16 +7,16 @@ import java.applet.*;
 public class Food extends Enemy{
 	
 	private static String[] tadimageNames = new String[]{"art/food/green_50h50w.png"};
-	private static String[] tadsoundNames = new String[]{};
+	private static String[] tadsoundNames = new String[]{"soundage/eating.wav"};
 	private static int[] tadimgval = new int[]{};
 	private static String[] hinimageNames = new String[]{"art/food/yellow_50h50w.png"};
-	private static String[] hinsoundNames = new String[]{};
+	private static String[] hinsoundNames = new String[]{"soundage/eating.wav"};
 	private static int[] hinimgval = new int[]{};
 	private static String[] nerimageNames = new String[]{"art/food/green_75h75w.png"};
-	private static String[] nersoundNames = new String[]{};
+	private static String[] nersoundNames = new String[]{"soundage/eating.wav"};
 	private static int[] nerimgval = new int[]{};
 	private static String[] frgimageNames = new String[]{"art/food/yellow_75h75w.png"};
-	private static String[] frgsoundNames = new String[]{};
+	private static String[] frgsoundNames = new String[]{"soundage/eating.wav"};
 	private static int[] frgimgval = new int[]{};
 	private Random rand = new Random();
 	private BufferedImage bi;
@@ -72,7 +72,7 @@ public class Food extends Enemy{
 	//this food sits still and dissappears when eaten
 	public void update(){}
 	public void kill(int condition){
-		//ac.play();
+		ac.play();
 	}
 	public void init(GameCore gc){
 		bi = gc.getImage(imgnm);
